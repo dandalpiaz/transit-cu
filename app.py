6e39dc19d4259cfdeabe2c10c16e416b0338a6f3
Dan Dalpiaz <dpdalpiaz@gmail.com>
2018-09-23 09:54:30 -0500

    add filter section to homepage, update styles

--- app.py
+++ app.py
@@ -19,7 +19,13 @@ def static_from_root_2():
 
 @app.route('/')
 def index():
-	return render_template('home.html')
+
+	all_stops = "https://developer.cumtd.com/api/v2.2/json/getroutes?key=f43367cb918d4110af23345fff93f294"
+
+	with urllib.request.urlopen(all_stops) as url:
+		all_stops_data = json.loads(url.read().decode())
+
+	return render_template('home.html', all_stops_data=all_stops_data)
 
 @app.route('/stop=<stop>')
 def get_stop(stop):
